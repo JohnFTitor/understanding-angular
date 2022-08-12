59e7c467bb194fb7a1df0fd36f1b146f92dcb9f4
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-08-12 12:37:27 -0500

    Create first component

---
+++ src/app/first-c/first-c.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-c',
+  templateUrl: './first-c.component.html',
+  styleUrls: ['./first-c.component.scss']
+})
+export class FirstCComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
